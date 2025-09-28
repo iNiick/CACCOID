3735b02310338de0c7027f08cd76835a07912d04
Guaricaya <rafaelbarros240@gmail.com>
2025-09-28 00:04:50 -0300

    [BACK] feat: materialização do Email service

---
+++ caccoId/src/main/java/br/com/cefet/caccoId/services/EmailService.java
@@ -0,0 +1,33 @@
+package br.com.cefet.caccoId.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public String sendEmail(String to, String subject, String message) {
+        // Implement email sending logic here using javaMailSender
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(from);
+            mailMessage.setTo(to);
+            mailMessage.setSubject(subject);
+            mailMessage.setText(message);
+            javaMailSender.send(mailMessage);
+            return "Email sent to " + to;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error sending email to " + to;
+        }
+        
+    }
+
+}
