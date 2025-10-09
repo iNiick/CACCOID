8f2b50da44229155f608eb8cd2a1f322ec1cbbdd
iNiick <55265752+iNiick@users.noreply.github.com>
2025-10-09 14:27:52 -0300

    Update useAPI.js

--- caccoid-front/src/hooks/useAPI.js
+++ caccoid-front/src/hooks/useAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const useAPI = (hasFile) => {
   const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_BASE_URL ||'http://localhost:8080',
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
       'Content-Type': hasFile ? 'multipart/form-data' : 'application/json',
