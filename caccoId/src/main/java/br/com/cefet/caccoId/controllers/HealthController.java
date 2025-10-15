e56614925b925036b38ac6ef88767ec691d6dd8d
iNiick <nickcalixto@gmail.com>
2025-10-15 06:16:24 -0300

    fix: package corrigido

--- caccoId/src/main/java/br/com/cefet/caccoId/controllers/HealthController.java
+++ caccoId/src/main/java/br/com/cefet/caccoId/controllers/HealthController.java
@@ -1,3 +1,4 @@
+package br.com.cefet.caccoId.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- caccoId/src/main/java/br/com/cefet/caccoId/infra/security/KeepAliveBackend.java
+++ caccoId/src/main/java/br/com/cefet/caccoId/infra/security/KeepAliveBackend.java
@@ -1,3 +1,4 @@
+package br.com.cefet.caccoId.infra.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
